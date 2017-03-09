744095204c02a5b21b708f039606e735b9f7c400
Роман <roman.sarder@yandex.ru>
2017-03-09 14:54:02 +0200

    TodoList component Todo component Render static data

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,35 @@
 const React = require('react');
+const TodoList = require('TodoList');
 
 const TodoApp = React.createClass({
+    getInitialState: function() {
+        return {
+            todos: [
+                {
+                    id: 1,
+                    text: 'Walk the dog'
+                },
+                {
+                    id: 2,
+                    text: 'Clean the yard'
+                },
+                {
+                    id: 3,
+                    text: 'Cook the dinner'
+                },
+                {
+                    id: 4,
+                    text: 'Finish project'
+                }
+            ]
+        };
+    },
     render: function() {
+        let {todos} = this.state;
+
         return (
             <div>
-                TodoApp.jsx
+                <TodoList todos={todos}/>                
             </div>
         );
     }
