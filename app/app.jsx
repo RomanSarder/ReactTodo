9eb4d68cb8bde21baca137aa763d60c09777d7a6
Роман <roman.sarder@yandex.ru>
2017-03-09 15:12:39 +0200

    Test TodoList

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const {Route, Router, IndexRoute, hashHistory} = require('react-router');
-const Main = require('Main');
 const TodoApp = require('TodoApp');
 
 
---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,26 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const expect = require('expect');
+const $ = require('jquery');
+const TestUtils = require('react-addons-test-utils');
+const TodoList = require('TodoList');
+const Todo = require('Todo');
+
+describe('TodoList', () => {
+    it('should exist', () => {
+        expect(TodoList).toExist();
+    });
+    it('should render one Todo component for each todo item', () => {
+        let todos = [{
+            id: 1,
+            text: 'Do something'
+        },
+        {
+            id: 2,
+            text: 'Check'
+        }];
+        let todoList = TestUtils.renderIntoDocument(<TodoList todos={todos} />);
+        let todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo );
+        expect(todosComponents.length).toBe(todos.length);
+    })
+});
