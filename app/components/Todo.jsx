4a92f37b665d43f7ad18e7f1e2fee8434501eafc
Роман <roman.sarder@yandex.ru>
2017-03-10 15:39:18 +0200

    Add and test Todo Toggling feature

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -1,11 +1,15 @@
 const React = require('react');
 
 const Todo= React.createClass({
+    handleClick: function() {
+        this.props.onToggle(this.props.id);
+    },
     render: function() {
-        let {text, id} = this.props;
+        let {text, id, completed} = this.props;
         return (
-            <div>
-                {`${id}. ${text}`}
+            <div onClick={this.handleClick}>
+                <input type="checkbox" checked={completed}/>
+                {text}
             </div>
         );
     }
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,22 +12,37 @@ const TodoApp = React.createClass({
             todos: [
                 {
                     id: uuid(),
-                    text: 'Walk the dog'
+                    text: 'Walk the dog',
+                    completed: false
                 },
                 {
                     id: uuid(),
-                    text: 'Clean the yard'
+                    text: 'Clean the yard',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Cook the dinner'
+                    text: 'Cook the dinner',
+                    completed: true
                 },
                 {
                     id: uuid(),
-                    text: 'Finish project'
+                    text: 'Finish project',
+                    completed: false
                 }
             ]
         };
+    },
+    handleToggle: function(id) {
+        let updateTodos = this.state.todos.map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+        this.setState({
+            todos: updateTodos
+        });
     },
     handleAddTodo: function(text) {
         this.setState({
@@ -35,7 +50,8 @@ const TodoApp = React.createClass({
                 ...this.state.todos,
                 {
                     id: uuid(),
-                    text: text
+                    text: text,
+                    completed: false
                 }
             ]
         })
@@ -52,7 +68,7 @@ const TodoApp = React.createClass({
         return (
             <div>
                 <TodoSearch onSearch={this.handleSearch}/>
-                <TodoList todos={todos}/>
+                <TodoList todos={todos} onToggle={this.handleToggle}/>
                 <AddTodo onAddTodo={this.handleAddTodo}/>               
             </div>
         );
